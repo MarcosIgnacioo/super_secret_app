8d9360e192186d25bf126d323c1acceb292cb00b
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-08-28 21:53:40 -0700

    weppp

--- apis/hello.go
+++ apis/hello.go
@@ -1,11 +1,67 @@
 package apis
 
 import (
+	"database/sql"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func GenericHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"esto": "functiona",
+	})
+}
+
+type User struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
+	Birthday string `json:"birthday"`
+}
+
+func SQL(c *gin.Context) {
+	db, err := sql.Open("sqlite3", "users.db")
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer db.Close()
+
+	var version string
+	err = db.QueryRow("SELECT SQLITE_VERSION()").Scan(&version)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	statement, err := db.Prepare(`CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(64) NULL,
+		password VARCHAR(64) NULL,
+		birthday DATE NULL)`)
+
+	if err != nil {
+		log.Println("Error in creating table")
+	} else {
+		log.Println("Successfully created table persona 5!")
+	}
+
+	statement.Exec()
+
+	// Create
+	statement, _ = db.Prepare("INSERT INTO users (name, password, birthday) VALUES (?, ?, ?)")
+	statement.Exec("Persona", "contra", "28-08-2024")
+	log.Println("Inserted the person into database!")
+	rows, _ := db.Query("SELECT id, name, password, birthday FROM users")
+
+	var user User
+	for rows.Next() {
+		rows.Scan(&user.ID, &user.Name, &user.Password, &user.Birthday)
+	}
+
+	c.JSON(200, gin.H{
+		"esto": user.Name,
 	})
 }
--- server.go
+++ server.go
@@ -25,5 +25,6 @@ func main() {
 		})
 	})
 	r.GET("/hello", apis.GenericHandler)
+	r.GET("/db", apis.SQL)
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
